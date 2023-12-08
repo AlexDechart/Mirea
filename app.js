5c9d7c99d1815f9bf33a9ff9d1d291f99ed571e3
ikenchik <i@kenchikk.ru>
2023-12-08 18:53:48 +0300

    Сервер написан

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+app.use(express.static(__dirname + "/Front/img"));
+app.use(express.static(__dirname + "/Front"));
+
+app.use("/main",function (_, response) {
+    response.redirect("/")
+  });
+
+app.get("/", function(request, response){
+    response.sendFile(__dirname + "/Front/index.html");
+});
+
+app.get("/reg.html", function(request, response){
+    response.sendFile(__dirname + "/Front/reg.html");
+});
+
+app.get("/lk.html", function(request, response){
+    response.sendFile(__dirname + "/Front/lk.html");
+});
+
+app.listen(2890, function(){console.log("Server started at: http://localhost:2890")});
